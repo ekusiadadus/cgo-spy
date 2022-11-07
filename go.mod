b7df4a92f9cc401752af41cdb267e1a61b86cc32
ekusiadadus <ekusiadadus@gmail.com>
2022-11-07 19:11:46 +0900

    feat: :books: add vspy's visualizer

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/cgo-spy
+
+go 1.19
+
+require (
+	github.com/markcheno/go-quote v0.0.0-20220624214117-555891babbf1
+	github.com/markcheno/go-talib v0.0.0-20190307022042-cd53a9264d70
+)
+
+require github.com/stretchr/testify v1.8.1 // indirect
--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import plotly.graph_objects as go
 df = pd.read_csv('spy.csv')
 fig = go.Figure()
 fig.add_trace(go.Candlestick(
-                x=df['datetime'],
-                open=df['open'],
-                high=df['high'],
-                low=df['low'],
-                close=df['close'],
-                increasing_line_color= 'red', decreasing_line_color= 'gray'
-                )
-             )
+    x=df['datetime'],
+    open=df['open'],
+    high=df['high'],
+    low=df['low'],
+    close=df['close'],
+    increasing_line_color='red', decreasing_line_color='gray'
+)
+)
 fig.update_xaxes(rangeslider_visible=False)
 fig.show()
+fig.write_html('spy.html')
